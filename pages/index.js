2c5a04d65fbc8bc56984a1dc23c187ae219c1c22
Isaac Teixeira de Oliveira <isaacbh0297@gmail.com>
2025-03-24 23:09:13 +0000

    centraliza a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Nádia, eu amo você. Se você me ama dá uma risadinha!</h1>
+    return (
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh'}}>
+            <h1 >Nádia, eu amo você. Se você me ama dá uma risadinha!</h1>
+        </div>
+    )
 }
 
 export default Home;
