e5a9400ebc36fc14c14b2e9e94a7872c3de7b1fb
Isaac Teixeira de Oliveira <isaacbh0297@gmail.com>
2025-03-29 00:54:13 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 function Home() {
-    return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh'}}>
-            <h1 style={{ textAlign: 'center' }}>Nádia, eu amo você. Se você me ama dá uma risadinha!</h1>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>
+        Nádia, eu amo você. Se você me ama dá uma risadinha!
+      </h1>
+    </div>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
