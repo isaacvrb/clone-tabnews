2951655791f5a3575a9f8f9e2bd69685c98a09e6
Isaac Teixeira <isaacbh0297@gmail.com>
2025-05-27 21:24:57 -0300

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import database from "infra/database";
+import password from "models/password.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function findOneByUsername(username) {
@@ -36,6 +37,7 @@ async function findOneByUsername(username) {
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -80,6 +82,11 @@ async function create(userInputValues) {
         action: "Utilize outro username para realizar o cadastro.",
       });
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPasword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPasword;
   }
 
   async function runInsertQuery(userInputValues) {
