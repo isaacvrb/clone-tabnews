6e2b7a82fb736c8b25cf42203de34afe1475b246
Isaac Teixeira <isaacbh0297@gmail.com>
2025-11-11 20:55:15 -0300

    feat: require `read:activation_token` to access `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -2,7 +2,8 @@ import user from "./user";
 import email from "infra/email.js";
 import database from "infra/database.js";
 import webserver from "infra/webserver.js";
-import { NotFoundError } from "infra/errors";
+import authorization from "./authorization";
+import { ForbiddenError, NotFoundError } from "infra/errors";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000;
 
@@ -92,6 +93,15 @@ async function markTokenAsUsed(activationTokenId) {
 }
 
 async function activateUserByUserId(userId) {
+  const userToActivate = await user.findOneById(userId);
+
+  if (!authorization.can(userToActivate, "read:activation_token")) {
+    throw new ForbiddenError({
+      message: "Você não pode mais utilizar tokens de ativação.",
+      action: "Entre em contato com o suporte.",
+    });
+  }
+
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
@@ -120,6 +130,7 @@ const activation = {
   sendEmailToUser,
   findOneValidById,
   activateUserByUserId,
+  EXPIRATION_IN_MILLISECONDS,
 };
 
 export default activation;
